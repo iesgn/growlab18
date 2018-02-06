05928727d68b1572ff7c8d86e636f93d8521f3e3
Jesús García Muñoz <jesus.garcia.inf@gmail.com>
2018-02-06 19:23:06 +0100

    Update funciones_jesus.py

--- Desarrollo/Jesus/funciones_jesus.py
+++ Desarrollo/Jesus/funciones_jesus.py
@@ -1,5 +1,6 @@
+from datetime import timedelta
 #Calcula los huecos que hay en un dia segun el intervalo indicado, representa cada hueco como *.
-def calcular_huecos(intervalo):
+def calcular_huecos_dia(intervalo):
 	lista = []
 	for elem in range(int((60 / intervalo) * 24)):
 		lista.append('*')
@@ -10,3 +11,25 @@ def programar_evento(nombre, periodo, prioridad, duracion, fecha_inicio, hora_in
 	lista = [{'nombre':nombre}, {'periodo':periodo}, {'prioridad':prioridad}, {'duracion':duracion}, {'fecha_inicio':fecha_inicio}, {'hora_inicio':hora_inicio}, 
 	{'fecha_fin':fecha_fin}, {'hora_fin':hora_fin}]
 	return lista
+
+#Calcula el número de candidatos posibles entre dos horas con un intervalo determinado.
+def calcular_candidatos(intervalo, hora_inicio, hora_fin):
+	rango_horas = timedelta(hours=int(hora_inicio.split(':')[0]), minutes=int(hora_inicio.split(':')[1]))
+	rango_horas_2 = timedelta(hours=int(hora_fin.split(':')[0]), minutes=int(hora_fin.split(':')[1]))
+	diferencia = abs(rango_horas.total_seconds() - rango_horas_2.total_seconds())
+	candidatos = diferencia / (intervalo * 60)
+	return candidatos
+
+intervalo = int(input('Intervalo: '))
+nombre = input('Nombre: ')
+periodo = input('Periodo: ')
+prioridad = input('Prioridad: ')
+duracion = input('Duración: ')
+fecha_inicio = input('Fecha inicio: ') 
+hora_inicio = input('Hora inicio: ')
+fecha_fin = input('Fecha fin: ')
+hora_fin = input('Hora fin: ')
+
+print (calcular_huecos_dia(intervalo))
+print (programar_evento(nombre, periodo, prioridad, duracion, fecha_inicio, hora_inicio, fecha_fin, hora_fin))
+print (calcular_candidatos(intervalo, hora_inicio, hora_fin))
