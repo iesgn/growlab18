1b9cd4f5fa30f6102d81d8e6947fa58ad6e912ea
Jesús García Muñoz <jesus.garcia.inf@gmail.com>
2018-02-06 18:51:27 +0100

    Update funciones_jesus.py

--- Desarrollo/Jesus/funciones_jesus.py
+++ Desarrollo/Jesus/funciones_jesus.py
@@ -1,6 +1,12 @@
-#Calcula los huecos que hay en un dia segun el intervalo indicado, representa cada hueco como '*'.
+#Calcula los huecos que hay en un dia segun el intervalo indicado, representa cada hueco como *.
 def calcular_huecos(intervalo):
 	lista = []
 	for elem in range(int((60 / intervalo) * 24)):
 		lista.append('*')
+	return lista
+
+#Programa un evento con las características indicadas.
+def programar_evento(nombre, periodo, prioridad, duracion, fecha_inicio, hora_inicio, fecha_fin, hora_fin):
+	lista = [{'nombre':nombre}, {'periodo':periodo}, {'prioridad':prioridad}, {'duracion':duracion}, {'fecha_inicio':fecha_inicio}, {'hora_inicio':hora_inicio}, 
+	{'fecha_fin':fecha_fin}, {'hora_fin':hora_fin}]
 	return lista
