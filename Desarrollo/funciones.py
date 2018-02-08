844368df2c9871419473eca26eba8b021f2ed50b
hijano94 <carloshijano@msn.com>
2018-02-08 23:30:30 +0100

    ampliacion de las funciones

--- Desarrollo/funciones.py
+++ Desarrollo/funciones.py
@@ -1,6 +1,7 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
-SEG_TEMP=5 	# constante de tiempo (EN MINUTOS) para "segmentar" el dia
+SEG_TEMP=5 				# constante de tiempo (EN MINUTOS) para "segmentar" el dia
+FORMATO = "%H:%M"		# Formato para la funcion datetime
 
 # Voy a hacer un fichero de texto con varios eventos para ahorrarme el escribirlo por pantalla
 # estructura del archivo: nombre, periodo, prioridad, duracion, fecha_inicio, fecha_fin, hora_inicio, hora_fin
@@ -15,43 +16,60 @@ def IngresarEventos():
 			dic["duracion"]=evento.split(",")[3]
 			dic["fecha_inicio"]=evento.split(",")[4]
 			dic["fecha_fin"]=evento.split(",")[5]
-			dic["hora_inicio"]=timedelta(hours=int(evento.split(",")[6].split(":")[0]),minutes=int(evento.split(",")[6].split(":")[1]))
+			dic["hora_inicio"]=datetime.strptime(evento.split(",")[6], FORMATO)
 			if evento.split(",")[7] != "hora_fin\n":
-				dic["hora_fin"]=timedelta(hours=int(evento.split(",")[7].split(":")[0]),minutes=int(evento.split(",")[7].split(":")[1]))
+				dic["hora_fin"]=datetime.strptime(evento.split(",")[7].split("\n")[0], FORMATO)
 			else:
-				dic["hora_fin"]="hora_fin"
-			eventos.append(dic)
+				dic["hora_fin"]=datetime.strptime(evento.split(",")[6], FORMATO)	# Si no tiene hora final, le asigno la hora 
+			eventos.append(dic)														# inicial para que se genere solo un candidato
 	return eventos
+
 # Genero la lista del tamano correspondiente a la constante 
 def GenerarTablaDia():		
 	dia=[]
 	for x in range(int((60/SEG_TEMP)*24)):
 		dia.append(None)
 	return dia
+
 # Funcion para imprimir la planificacion del dia	
 def ImprimirDia(*args):
-	hora,minuto=0,0
 	for x in args:
-		print("%.2i:%.2i ==> %s"%(hora,minuto,x))
-		minuto+=SEG_TEMP
-		if minuto==60:
-			hora+=1
-			minuto=0
-# Funcion para calcular el numero de candidatos de un evento			
+		print("%s:%s ==> %s"%(x["hora_inicio"].hour,x["hora_inicio"].minute,x["nombre"]))
+
+# Funcion para calcular el numero de candidatos de un evento | Retorna un entero			
 def CalcularCandidatos(**kwargs):
-	candidatos = abs(kwargs["hora_inicio"].total_minutes() - kwargs["hora_fin"].total_minutes())/SEG_TEMP
-	return candidatos
-# Genero una lista de diccionarios con todos los candidatos. Con la hora inicial modificada en cada candidato
+	candidatos = abs((kwargs["hora_inicio"].hour*60 + kwargs["hora_inicio"].minute) - (kwargs["hora_fin"].hour*60 + kwargs["hora_fin"].minute - int(kwargs["duracion"])))/SEG_TEMP
+	return int(candidatos+1)		
+
+# Genero una lista de diccionarios con todos los candidatos. Con la hora inicial modificada en cada candidato | Retorna una lista de diccionarios
 def GenerarCandidatos(**kwargs):
 	lista=[]
-	for evento in kwargs:
-		for x in range (1,CalcularCandidatos(evento)):
-			dic=evento
-			dic["hora_inicio"]=dic["hora_inicio"].total_minutes()+(SEG_TEMP*x)
+	for x in range (0,CalcularCandidatos(**kwargs)):
+		dic=kwargs
+		if x==0:
+			lista.append(dic)
+		else:
+			dic["hora_inicio"]+=timedelta(minutes=(SEG_TEMP*x))		# Esta claro que esto no funciona como quiero
 			lista.append(dic)
 	return lista			
-# Rellena la lista con todos los candidatos ordenados por tiempo / prioridad 			
-def TablaTemPri(*args,**kwargs):
+
+# Rellena la lista con todos los candidatos ordenados por tiempo / prioridad | Retorna una lista de listas de diccionarios			
+def TablaTemPri(**kwargs):
 	tempri=[]
+	priUno=[]
+	priDos=[]
+	priTres=[]
 	for evento in kwargs:
-		
+		if evento["prioridad"]=="1":
+			priUno.append(GenerarCandidatos(**evento))
+		elif evento["prioridad"]=="2":
+			priDos.append(GenerarCandidatos(**evento))	
+		elif evento["prioridad"]=="3":
+			priTres.append(GenerarCandidatos(**evento))	
+	tempri.append(priUno)
+	tempri.append(priDos)
+	tempri.append(priTres)		
+	return tempri
+
+eventos=IngresarEventos()
+ImprimirDia(*GenerarCandidatos(**eventos[3]))
